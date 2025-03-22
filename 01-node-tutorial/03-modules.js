10b6ceef12d7c6abe403bfc4ff24bea315eb4b49
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2025-03-21 22:53:47 -0400

    adding YT notes

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,5 +1,6 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
+//everytime you create your own module, you will start with ./
 const names = require('./04-names')
 const sayHi = require('./05-utils')
 const data = require('./06-alternative-flavor')
@@ -7,3 +8,5 @@ require('./07-mind-grenade')
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
+
+// other way is to  destructure : const {john} = require('./04-names)
