6528744b5e385e807a34d87097212c1eb1f9cfc5
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2025-03-21 22:37:22 -0400

    added notes

--- 01-node-tutorial/01-intro.js
+++ 01-node-tutorial/01-intro.js
@@ -7,3 +7,12 @@ if (amount < 10) {
 }
 
 console.log(`hey it's my first node app!!!`)
+
+// YT Video Notes:
+// Globals - NO WINDOW
+
+// _dirname - path to current directory 
+// _filename - file name 
+// require - function to use modules
+// module - info about current module (file)
+// process - info about env where the program is being executed 
